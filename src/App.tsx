e55deb2fc56430c2bd6f420508c435ce46ec7672
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-04-18 13:21:27 +0530

    Challenge Completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,29 @@
-import ComponentFactory from "./utils/ComponentFactory";
+import ComponentFactory, { ComponentConfig } from "./utils/ComponentFactory";
 
-const buttonConfig = {};
+const buttonConfig: ComponentConfig = {
+  type: "button",
+  props: {
+    label: "Click Me",
+    onClick: () => alert("You Clicked Me"),
+    disabled: false,
+  },
+};
+
+const cardConfig: ComponentConfig = {
+  type: "card",
+  props: {
+    title: "Card Title",
+    content: "Some content here.",
+    footer: "Footer",
+  },
+};
 
 const App = () => {
-  return <div>{ComponentFactory()}</div>;
+  return (
+    <div>
+      {ComponentFactory(buttonConfig)} {ComponentFactory(cardConfig)}
+    </div>
+  );
 };
 
 export default App;
