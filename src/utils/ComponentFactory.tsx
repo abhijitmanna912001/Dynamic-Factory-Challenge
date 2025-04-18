37c6243ff19397fbfadee319c574e2bd0f0655b0
Abhijit Manna <79353253+abhijitmanna912001@users.noreply.github.com>
2025-04-18 13:03:27 +0530

    ComponentFactory completed

---
+++ src/utils/ComponentFactory.tsx
@@ -0,0 +1,24 @@
+import Button from "../components/Button";
+import Card from "../components/Card";
+import Modal from "../components/Modal";
+import { ButtonProps, CardProps, ModalProps } from "./component-types";
+
+type ComponentConfig =
+  | { type: "button"; props: ButtonProps }
+  | { type: "card"; props: CardProps }
+  | { type: "modal"; props: ModalProps };
+
+function ComponentFactory(config: ComponentConfig) {
+  switch (config.type) {
+    case "button":
+      return <Button {...config.props} />;
+    case "card":
+      return <Card {...config.props} />;
+    case "modal":
+      return <Modal {...config.props} />;
+    default:
+      throw new Error("Unknown component type");
+  }
+}
+
+export default ComponentFactory;
